a365b1d712c2040ef5a84179bdbdef414b12bee1
AlienAmigo <maestro_pete@inbox.ru>
2025-11-11 12:53:51 +0300

    api url added

---
+++ src/config/api.ts
@@ -0,0 +1,29 @@
+export const BASE_URL = 'https://cinemaguide.skillbox.cc';
+
+// AUTH
+export const AUTH_LOGIN_URL = '/auth/login';
+export const AUTH_LOGOUT_URL = '/auth/logout';
+export const USER_CREATE_URL = '/user';
+export const PROFILE_URL = '/profile';
+
+// FAVORITES
+export const FAVORITES_URL = '/favorites';
+
+// MOVIE
+GET
+/movie
+Получение фильмов по заданным фильтрам
+GET
+/movie/top10
+Получение фильмов с наивысшим рейтингом
+GET
+/movie/genres
+Получение жанров
+GET
+/movie/{movieId}
+Получение фильма по id
+GET
+/movie/random
+Получение случайного фильма
+
+ */
---
+++ src/types/schemas/index.ts
@@ -0,0 +1,65 @@
+export interface IUser {
+  name?: string;
+  surname?: string;
+  email?: string;
+  favorites?: (string | number)[];
+}
+
+export interface IAuthInfo {
+  email: string;
+  password: string;
+}
+
+export interface ISuccessfulResult {
+  result: boolean;
+}
+
+export interface IError {
+  error: string;
+}
+
+export interface IRegisterData {
+  email: string;
+  password: string;
+  name?: string;
+  surname?: string;
+}
+
+export interface IMovie {
+  id: number;
+  title: string;
+  originalTitle: string;
+  language: string;
+  relaseYear: number;
+  releaseDate: string;
+  genres: string[];
+  plot: string;
+  runtime: number;
+  budget: string;
+  revenue: string;
+  homepage: string;
+  status: string;
+  posterUrl: string;
+  backdropUrl: string;
+  trailerUrl: string;
+  trailerYoutubeId: string;
+  tmdbRating: number;
+  searchL: string;
+  keywords: string[];
+  countriesOfOrigin: string[];
+  languages: string[];
+  cast: string[];
+  director: string;
+  production: string;
+  awardsSummary: string;
+}
+
+export interface IApiResponse {
+  code: number;
+  type: string;
+  message: string;
+}
+
+export interface IFavoritesBody {
+  id: string;
+}
